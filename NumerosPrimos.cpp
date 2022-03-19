b7ca2ea9f54b0294daad995e5f9206e08b163b5e
HectorMau501 <mauhector7@gmail.com>
2022-03-19 14:24:29 -0600

    Desarrollo de programa

--- NumerosPrimos.cpp
+++ NumerosPrimos.cpp
@@ -14,7 +14,17 @@ using namespace std;
 //Funcion principal
 int main(int argc, char** argv) {
 
+	//Declaracion de varibles locales
+	int i, n=0, cambio=0;
+	
+	cout<<"Bienvenido"<<endl;
+	cout<<"Hector Mauricio Rodriguez Salazar 21310314"<<endl;
+	cout<<"Numeros Primos del 1 hasta n"<<endl;
+	cout<<"Coloque el numero que desea imprimir desde el 1 hasta n en primos"<<endl;
+	cin>>n; //Guardamos el numero aqui
+	cout<<"Los numeros pares son"<<endl;
 
 
-    
+
+    return 0;
 }
