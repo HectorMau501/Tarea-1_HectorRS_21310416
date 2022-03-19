50c78dc7c34c8923a6aac9e60be3b87e7c359a6b
HectorMau501 <mauhector7@gmail.com>
2022-03-19 14:17:27 -0600

    Inicio

---
+++ NumerosPrimos.cpp
@@ -0,0 +1,20 @@
+/*Ceti Colomos
+Docente: Luis Manuel Morales Medina
+Hector Mauricio Rodriguez Salazar 21310416
+Grupo y Grado 2P2*/
+
+
+
+//Bibliotecas
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//Funcion principal
+int main(int argc, char** argv) {
+
+
+
+    
+}
