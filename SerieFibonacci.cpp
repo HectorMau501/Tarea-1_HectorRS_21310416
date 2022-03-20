d6de1b1f11cf682e26519bd99fd6a7e7b4981d48
HectorMau501 <mauhector7@gmail.com>
2022-03-19 18:02:26 -0600

    Incio del Programa de SerieFibonacci

---
+++ SerieFibonacci.cpp
@@ -0,0 +1,21 @@
+/*Ceti Colomos
+Docente: Luis Manuel Morales Medina
+Hector Mauricio Rodriguez Salazar 21310416
+Grupo y Grado 2P2*/
+
+
+//Bibliotecas
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+
+//Funcion principal
+int main(int argc, char** argv) {
+
+
+
+    system("pause");
+	return 0;
+}
