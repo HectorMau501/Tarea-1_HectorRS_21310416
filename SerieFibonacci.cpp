7ff36210429e2c6ed22658e363b05baaa6626fc3
HectorMau501 <mauhector7@gmail.com>
2022-03-19 18:08:21 -0600

    Agregado de Contenido al ProgramaFibonacci

--- SerieFibonacci.cpp
+++ SerieFibonacci.cpp
@@ -14,6 +14,15 @@ using namespace std;
 //Funcion principal
 int main(int argc, char** argv) {
 
+    //Variables de tipo local
+	int numero, i, x=0, y=1, z=1;
+	
+	
+	cout<<"\t\tBienvenido"<<endl;
+	cout<<"\t\tHector Mauricio Rodriguez Salazar 21310314"<<endl;
+	cout<<"\n\t\tSerie fibonacci "<<endl;
+	cout<<"Inserte el numero hasta donde desea que llegue la serie fibonacci"<<endl;
+	cin>>numero;
 
 
     system("pause");
