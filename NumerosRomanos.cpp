5ab76749615fa5a0932fd4fea42519b520dfd29a
HectorMau501 <mauhector7@gmail.com>
2022-03-19 18:26:19 -0600

    Inicio del ProgramaNumerosRomanos

---
+++ NumerosRomanos.cpp
@@ -0,0 +1,24 @@
+/*Ceti Colomos
+Docente: Luis Manuel Morales Medina
+Hector Mauricio Rodriguez Salazar 21310416
+Grupo y Grado 2P2*/
+
+//Bibliotecas
+
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+
+
+
+
+    
+	system("pause");
+}
+
+
